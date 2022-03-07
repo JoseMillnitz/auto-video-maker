8c763c49fdcb07b81ddaee3b06fb4746f43e2d8a
José Millnitz <lolpqn@gmail.com>
2022-03-06 23:25:15 -0300

    adicionado variavel "outputtxt" e escrita em txt
    agora coloquei que tambem vai ser atualizado em um arquivo, usando a variavel outputtxt, para poder utilizar em outra parte do programa depois
    e eu reescrevo toda ver que eu uso

--- silabas/texto.py
+++ silabas/texto.py
@@ -6,12 +6,16 @@ dic = pyphen.Pyphen(lang='pt_BR')
 
 #criando uma variavel para ler de um texto
 f = open("textoprincipal.txt", 'r')
+#variavel para escrever em um arquivo depois
+outputtxt = open("OUTPUT.txt", "w")
 
 #criando um loop em que enquanto ler uma palavra do texto, vai jogar no dicionario
 #e vai printar tudo que estiver dentro do arquivo de texto já separado em silabas
+#por ultimo esta adicionando a um arquivo de texto par uso futuro
 for word in f:
     awoo = dic.inserted(word)
     print(awoo)
-
+    outputtxt.write(awoo)
+    outputtxt.close()
 print("")
 input("de enter para sair")
